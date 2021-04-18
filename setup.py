782b00ab4c80c1d2b122d63ff1be8802ed7f0c0b
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2021-04-18 19:44:44 +0200

    drop support for python 3.3/3.4

--- setup.py
+++ setup.py
@@ -48,16 +48,17 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'License :: OSI Approved :: GNU General Public License (GPL)'
     ],
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
     extras_require={
         'build': [
             'Jinja2==2.7.2',
