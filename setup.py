26fe220d6a71f8e45f62089a2c8cf4bd008c9fbd
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2021-04-20 19:38:30 +0200

    drop python 2 support

--- setup.py
+++ setup.py
@@ -45,8 +45,6 @@ setup(
         'Intended Audience :: System Administrators',
         'Natural Language :: English',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -58,7 +56,7 @@ setup(
     ],
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
+    python_requires='>=3.6',
     extras_require={
         'build': [
             'Jinja2==2.7.2',
